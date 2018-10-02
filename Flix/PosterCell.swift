1f5334145319d3974e1b87f542424565ca4adc37
Sandaire <sandynasj@icloud.com>
2018-10-01 23:59:58 -0400

    collectionView

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,17 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Admin on 10/1/18.
+//  Copyright © 2018 Sandyna Sandaire Jerome. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    
+}
